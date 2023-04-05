b90513d723da4c4e727acaa276bd03f99667c7e0
mstamenkovski27@gmail.com <mstamenkovski27@gmail.com>
2023-04-05 19:11:30 +0200

    Homework1

--- Homework1/Homework1/Program.cs
+++ Homework1/Homework1/Program.cs
@@ -2,10 +2,10 @@
 
 // Create a dog and call its methods
 Dog dog = new Dog("Debel", 5);
-dog.PrintAnimal(); // Output: I am a dog named Fido and I am 5 years old.
-dog.Bark(); // Output: Woof woof!
+dog.PrintAnimal(); 
+dog.Bark(); 
 
 // Create a cat and call its methods
 Cat cat = new Cat("Mara", 3);
-cat.PrintAnimal(); // Output: I am a cat named Fluffy and I am 3 years old.
-cat.Eat("fish"); // Output: I am eating fish.
+cat.PrintAnimal(); 
+cat.Eat("fish"); 
